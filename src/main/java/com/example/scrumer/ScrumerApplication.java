b163acd80775ae5fa67c901fb18d421f0052ff19
Kinga <wronakinga@gmail.com>
2021-03-15 20:03:05 +0100

    creating a project in java / spring boot

---
+++ src/main/java/com/example/scrumer/ScrumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.scrumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scrumer/ScrumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scrumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
