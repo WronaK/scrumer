88d6be5262407ba32f558ec0cf8c38f5330443ce
Kinga <wronakinga@gmail.com>
2021-03-23 08:21:27 +0100

    add the possibility of creating a project and adding yourself to the team, modify styles

--- src/web-ui/src/app/app-routing.module.ts
+++ src/web-ui/src/app/app-routing.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MainLayoutComponent} from "./main-layout/main-layout.component";
 import {ProductBacklogComponent} from "./product-backlog/product-backlog.component";
-import {LoginPageComponent} from "./authorization/login-page/login-page.component";
-import {RegistrationPageComponent} from "./authorization/registration-page/registration-page.component";
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {RegistrationPageComponent} from "./registration-page/registration-page.component";
 import {SprintBacklogComponent} from "./sprint-backlog/sprint-backlog.component";
 import {HomePageComponent} from "./home-page/home-page.component";
+import {JoinTeamComponent} from "./join-team/join-team.component";
 
 const routes: Routes = [
   { path: '', component: MainLayoutComponent, children: [
-      { path: 'product-backlog', component: ProductBacklogComponent },
-      { path: 'sprint-backlog', component: SprintBacklogComponent },
+      { path: 'product-backlog/:id', component: ProductBacklogComponent },
+      { path: 'sprint-backlog/:id', component: SprintBacklogComponent },
       { path: 'home', component: HomePageComponent}
     ]},
-  { path: 'login-page', component: LoginPageComponent},
-  { path: 'registration-page', component: RegistrationPageComponent},
+  { path: 'login', component: LoginPageComponent},
+  { path: 'registration', component: RegistrationPageComponent},
+  { path: 'join-team', component: JoinTeamComponent},
 ];
 
 @NgModule({
--- src/web-ui/src/app/app.module.ts
+++ src/web-ui/src/app/app.module.ts
@@ -9,13 +9,12 @@ import { MaterialModule } from './material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatSelectModule} from "@angular/material/select";
-import { LoginPageComponent } from './authorization/login-page/login-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import { RegistrationPageComponent } from './authorization/registration-page/registration-page.component';
 import {MatMenuModule} from "@angular/material/menu";
-import { SprintBacklogComponent } from './sprint-backlog/sprint-backlog.component';
-import {MatExpansionModule} from "@angular/material/expansion";
-import { TaskStatesComponent } from './task-states/task-states.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { JoinTeamComponent } from './join-team/join-team.component';
 
 
 @NgModule({
@@ -26,7 +25,9 @@ import { TaskStatesComponent } from './task-states/task-states.component';
     LoginPageComponent,
     RegistrationPageComponent,
     SprintBacklogComponent,
-    TaskStatesComponent
+    TaskStatesComponent,
+    HomePageComponent,
+    JoinTeamComponent
   ],
   imports: [
     BrowserModule,
