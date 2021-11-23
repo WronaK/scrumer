67539774a48d81b30ce9bf93b22c7a1b589224d2
kinga <kingaewelina@gmail.com>
2021-11-23 08:11:04 +0100

    add the ability to download files

--- src/main/java/com/example/scrumer/upload/controller/UploadController.java
+++ src/main/java/com/example/scrumer/upload/controller/UploadController.java
@@ -1,7 +1,5 @@
 package com.example.scrumer.upload.controller;
 
-import com.example.scrumer.project.entity.Project;
-import com.example.scrumer.project.service.useCase.ProjectUseCase;
 import com.example.scrumer.upload.service.useCase.UploadUseCase;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.webjars.NotFoundException;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -28,29 +25,26 @@ import java.time.LocalDateTime;
 @RequestMapping("/api/uploads")
 public class UploadController {
     private final UploadUseCase uploadUseCase;
-    private final ProjectUseCase projectUseCase;
 
-//    @GetMapping("/{id}")
-//    public UploadResponse getUpload(@PathVariable String id) {
-//        return uploadUseCase.getById(id).map(uploadFile ->
-//            new UploadResponse(uploadFile.getId(), uploadFile.getContentType(), uploadFile.getFilename(), uploadFile.getCratedAt()))
-//                .orElseThrow(() -> new NotFoundException("Not found file with id: " + id));
-//    }
-//
-//    @GetMapping("/{id}/file")
-//    public ResponseEntity<Resource> getUploadFile(@PathVariable String id) {
-//        return uploadUseCase.getById(id).map(uploadFile -> {
-//            String contentDisposition = "attachment; filename=\"" + uploadFile.getFilename() + "\"";
-//            byte[] bytes = uploadFile.getFile();
-//            Resource resource = new ByteArrayResource(bytes);
-//            return ResponseEntity
-//                    .ok()
-//                    .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
-//                    .contentType(MediaType.parseMediaType(uploadFile.getContentType()))
-//                    .body(resource);
-//
-//        }).orElseThrow(() -> new NotFoundException("Not found file with id: " + id));
-//    }
+    @GetMapping("/{id}/file")
+    public ResponseEntity<Resource> getUploadFile(@PathVariable Long id) {
+        return uploadUseCase.getById(id).map(uploadFile -> {
+            String contentDisposition = "attachment; filename=\"" + uploadFile.getFilename() + "\"";
+            byte[] bytes = new byte[0];
+            try {
+                bytes = Files.readAllBytes(Path.of(uploadFile.getPath()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Resource resource = new ByteArrayResource(bytes);
+            return ResponseEntity
+                    .ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                    .contentType(MediaType.parseMediaType(uploadFile.getContentType()))
+                    .body(resource);
+
+        }).orElseThrow(() -> new NotFoundException("Not found file with id: " + id));
+    }
 
     @GetMapping("/{id}/image")
     public ResponseEntity<?> getImage(@PathVariable Long id)  {
